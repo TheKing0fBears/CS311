8e631355baf59827ec814a9237155f60b484ddbb
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-11-20 21:54:01 -0800

    Create core.cpp

---
+++ core.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+struct Node
+{
+  String name;
+  double weight;
+  Node children[];
+};
+
+int main()
+{
+ return 0; 
+}
